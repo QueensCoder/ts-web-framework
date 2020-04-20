06be6b912c2716bc5d1320e77141b9d8c3f858b9
Ozal Khan <ozalkhan196@gmail.com>
2020-04-20 13:24:04 -0400

    created event handler for frame work

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-import { User } from '../models/User';
+import { User } from './models/User';
+
+const user = new User({ name: 'one', age: 2 });
+
+user.on('change', () => {
+  console.log('chage one');
+});
+
+user.on('change', () => {
+  console.log('chage two');
+});
+
+console.log(user.events);
+
+user.trigger('change');
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,14 +1,34 @@
 // interface also cleans up code by not having a ton
 // code in the constructor args
 interface UserProps {
-  name: string;
-  age: number;
+  name?: string;
+  age?: number;
 }
 
+type Callback = () => void;
+
 export class User {
+  events: { [key: string]: Callback[] } = {};
   constructor(private data: UserProps) {}
 
   get(propName: string): number | string {
     return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+    if (!handlers || !handlers.length) return;
+
+    handlers.forEach((callback) => callback());
   }
 }
