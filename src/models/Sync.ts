658a5efe12348baedc15b2d91922c306a0dedfcb
Ozal Khan <ozalkhan196@gmail.com>
2020-04-20 17:15:37 -0400

    sync class was refactored to return promises via axios promise

---
+++ src/models/Sync.ts
@@ -0,0 +1,16 @@
+import axios, { AxiosPromise } from 'axios';
+import { UserProps } from './User';
+
+export class Sync {
+  constructor(public rootUrl: string) {}
+
+  fetch(id: number): AxiosPromise {
+    return axios.get(`${this.rootUrl}/${id}`);
+  }
+
+  save(data: UserProps): AxiosPromise<void> {
+    const { id } = data;
+    if (id) axios.put(`${this.rootUrl}/${id}`, data);
+    else axios.post(`${this.rootUrl}`, data);
+  }
+}
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,9 +1,8 @@
-import axios, { AxiosResponse } from 'axios';
 import { Eventing } from './Eventing';
 
 // interface also cleans up code by not having a ton
 // code in the constructor args
-interface UserProps {
+export interface UserProps {
   name?: string;
   age?: number;
   id?: number;
@@ -21,21 +20,5 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  async fetch(): Promise<void> {
-    const resp: AxiosResponse = await axios.get(
-      `http://localhost:3000/users/${this.get('id')}`
-    );
-    this.set(resp.data);
-  }
-
-  async save(): Promise<void> {
-    if (this.get('id'))
-      await axios.put(
-        `http://localhost:3000/users/${this.get('id')}`,
-        this.data
-      );
-    else await axios.post(`http://localhost:3000/users`, this.data);
   }
 }
