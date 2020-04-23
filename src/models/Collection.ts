61aa7d7c08ae5206159675790f6b20010c43455a
Ozal Khan <ozalkhan196@gmail.com>
2020-04-23 16:48:18 -0400

    major refactor on collections now able to use collections for other information besides users

--- src/models/Collection.ts
+++ src/models/Collection.ts
@@ -1,12 +1,16 @@
 import axios, { AxiosResponse } from 'axios';
-import { User, UserProps } from './User';
 import { Eventing } from './Eventing';
 
-export class Collection {
-  models: User[] = [];
+//    a class with two generics
+// t is the class type of the collection aka user blog post etc
+// k is the props associated with the collection type
+export class Collection<T, K> {
+  models: T[] = [];
   events: Eventing = new Eventing();
 
-  constructor(public rootUrl: string) {}
+  //   deserialize takes in json in that satisfies K and returns an instance of T
+  //   now our fetch can work with any build method and not only User.build
+  constructor(public rootUrl: string, public deserialize: (json: K) => T) {}
 
   get on() {
     return this.events.on;
@@ -19,9 +23,8 @@ export class Collection {
   async fetch(): Promise<void> {
     try {
       const { data }: AxiosResponse = await axios.get(this.rootUrl);
-      data.forEach((userData: UserProps): void => {
-        const user = User.buildUser(userData);
-        this.models.push(user);
+      data.forEach((value: K): void => {
+        this.models.push(this.deserialize(value));
       });
     } catch (err) {
       console.log(err);
