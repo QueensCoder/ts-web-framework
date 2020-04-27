b65363365a27b7a02a4beac720c55d6be639e48f
Ozal Khan <ozalkhan196@gmail.com>
2020-04-27 16:32:18 -0400

    added set age method on user model to set a random age, need to be able to re-render dom when a change is made

--- src/views/UserForm.ts
+++ src/views/UserForm.ts
@@ -1,34 +1,38 @@
+import { User } from '../models/User';
+
 export class UserForm {
-  constructor(public parent: Element) {}
+  constructor(public parent: Element, public model: User) {}
   template(): string {
     return `
     <div>
         <h1>User Form</h1>
+        <div>User name: ${this.model.get('name')}</div>
+        <div>User age: ${this.model.get('age')}</div>
         <input/>
         <Button>Click me</Button>
+        <Button class="set-age">Set random age</Button>
     </div>
     `;
   }
 
   eventsMap(): { [key: string]: () => void } {
     return {
-      'click:button': this.onButtonClick,
-      'mouseover:h1': this.onHover,
+      //   finds a class with name from using query selectorall
+      'click:.set-age': this.onSetAgeClick,
     };
   }
 
-  onHover(): void {
-    console.log('hoevered over h1');
-  }
-
-  onButtonClick(): void {
-    console.log('hello there from button');
-  }
+  //   had to bind onSetAge or else loose context of this
+  onSetAgeClick = (): void => {
+    //   able to access model form the user class
+    this.model.setRandomAge();
+  };
 
   //   binds events to html tags
   bindEvents(fragment: DocumentFragment): void {
     const eventsMap = this.eventsMap();
     for (let key in eventsMap) {
+      // splits the keys and uses the key and eventname to bind to the element
       const [eventName, selector] = key.split(':');
       fragment.querySelectorAll(selector).forEach((element) => {
         element.addEventListener(eventName, eventsMap[key]);
@@ -36,6 +40,9 @@ export class UserForm {
     }
   }
 
+  //   works like a pseudo react.render
+  // creates a document template and then binds the events to the template
+  // finally
   render(): void {
     const templateElement = document.createElement('template');
     templateElement.innerHTML = this.template();
